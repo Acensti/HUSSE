a622f957adb8a1fa0bd413593439b77828801040
Acensti <37810260+Acensti@users.noreply.github.com>
2023-06-20 02:14:52 +0200

    ,.

--- scripts.js
+++ scripts.js
@@ -1,14 +1,6 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    const portfolioImages = Array.from(document.querySelectorAll(".portfolio-image"));
-
-    function changeBackgroundImage() {
-        const currentBackgroundIndex = Math.floor(Math.random() * portfolioImages.length);
-        const image = portfolioImages[currentBackgroundIndex];
-        document.body.style.backgroundImage = `url('${getComputedStyle(image).backgroundImage.slice(4, -1)}')`;
-    }
-
-    setInterval(changeBackgroundImage, 5000); // Change every 5 seconds
-
-    // Initial background image
-    changeBackgroundImage();
+$(document).ready(function() {
+    // When the navbar's button is clicked, toggle the "show" class on the navbar's collapse div
+    $('.navbar-toggler').on('click', function() {
+        $('.navbar-collapse').toggleClass('show');
+    });
 });
